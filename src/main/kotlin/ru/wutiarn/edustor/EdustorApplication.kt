984fffaaffb37512760a83e6105c0772f77b85ba
Wutiarn <me@wutiarn.ru>
2016-02-22 22:23:27 +0300

    Init

---
+++ src/main/kotlin/ru/wutiarn/edustor/EdustorApplication.kt
@@ -0,0 +1,11 @@
+package ru.wutiarn.edustor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+/**
+ * Created by wutiarn on 22.02.16.
+ */
+@SpringBootApplication
+open class EdustorApplication {
+
+}
---
+++ src/main/kotlin/ru/wutiarn/edustor/main.kt
@@ -0,0 +1,7 @@
+package ru.wutiarn.edustor
+
+import org.springframework.boot.SpringApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(EdustorApplication::class.java, *args)
+}
