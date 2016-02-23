2dec3c58fcfb04235a5e19b9e0d191788dbb26b2
Wutiarn <me@wutiarn.ru>
2016-02-23 12:00:01 +0300

    Uploading Example

--- src/main/kotlin/ru/wutiarn/edustor/ApiController.kt
+++ src/main/kotlin/ru/wutiarn/edustor/ApiController.kt
@@ -1,18 +1,23 @@
 package ru.wutiarn.edustor
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.mongodb.gridfs.GridFsOperations
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
 import ru.wutiarn.edustor.models.User
 import ru.wutiarn.edustor.repository.UserRepository
+import javax.servlet.http.HttpServletRequest
 
 /**
  * Created by wutiarn on 22.02.16.
  */
 @RestController
 @RequestMapping("/api")
-open class ApiController @Autowired constructor(val repo: UserRepository) {
+open class ApiController @Autowired constructor(val repo: UserRepository, val gfs: GridFsOperations) {
+
 
     @RequestMapping("/register")
     fun register(@RequestParam login: String, @RequestParam password: String): String {
@@ -38,5 +43,10 @@ open class ApiController @Autowired constructor(val repo: UserRepository) {
     fun deregister(@RequestParam login: String): String {
         repo.deleteByLogin(login)
         return "Successfully deregistered"
+    }
+
+    @RequestMapping(value = "/upload", method = arrayOf(RequestMethod.POST))
+    fun upload(req: HttpServletRequest, @RequestParam file: MultipartFile) {
+        gfs.store(file.inputStream, "upload_${file.originalFilename}")
     }
 }
