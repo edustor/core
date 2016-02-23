1c1f037d09ab01d3c4848586859c2019e9e15ec5
Wutiarn <me@wutiarn.ru>
2016-02-23 13:03:37 +0300

    Sessions

---
+++ src/main/kotlin/ru/wutiarn/edustor/api/LoginController.kt
@@ -0,0 +1,45 @@
+package ru.wutiarn.edustor.api
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import ru.wutiarn.edustor.models.Session
+import ru.wutiarn.edustor.models.User
+import ru.wutiarn.edustor.repository.UserRepository
+
+/**
+ * Created by wutiarn on 23.02.16.
+ */
+@RestController
+@RequestMapping("/api/login")
+class LoginController @Autowired constructor(val repo: UserRepository) {
+    @RequestMapping(method = arrayOf(RequestMethod.POST))
+    fun login(@RequestParam login: String, @RequestParam password: String): String {
+        val user = repo.findByLogin(login)
+        if (user != null && user.password == password) {
+            val session = Session()
+            user.sessions.add(session)
+            repo.save(user)
+            return "Logged in as $login with token ${session.token.toString()}"
+        }
+        return "Login error"
+    }
+
+    @RequestMapping("/register")
+    fun register(@RequestParam login: String, @RequestParam password: String): String {
+        if (repo.countByLogin(login) > 0) {
+            return "Already exists"
+        }
+
+        val user = User(login, password)
+        repo.save(user)
+        return "Registered $login $password"
+    }
+
+    @RequestMapping("/check_token")
+    fun checkToken(): String {
+        return "Check is not implemented"
+    }
+}
--- src/main/kotlin/ru/wutiarn/edustor/models/User.kt
+++ src/main/kotlin/ru/wutiarn/edustor/models/User.kt
@@ -1,6 +1,7 @@
 package ru.wutiarn.edustor.models
 
 import org.springframework.data.annotation.Id
+import java.util.*
 
 /**
  * Created by wutiarn on 22.02.16.
@@ -8,5 +9,10 @@ import org.springframework.data.annotation.Id
 data class User(
         var login: String = "",
         var password: String = "",
-        @Id
-        var id: String? = null)
+        var sessions: MutableList<Session> = mutableListOf(),
+        @Id var id: String? = null
+)
+
+data class Session(
+        val token: String = UUID.randomUUID().toString()
+)
