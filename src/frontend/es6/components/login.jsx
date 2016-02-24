53002e9c9fa6aa91bf3dc4d5ed60988685d84fa3
wutiarn <wutiarn@wutiarn-nb.lan>
2016-02-24 21:12:04 +0300

    Working on login page

--- src/frontend/es6/components/login.jsx
+++ src/frontend/es6/components/login.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import TextField from 'material-ui/lib/text-field';
 import RaisedButton from 'material-ui/lib/raised-button';
+import {Link} from 'react-router'
 
 
 class Login extends React.Component {
+    login = () => {
+        alert("Logging in...")
+    };
+
     render = () => {
         return <div>
-            Login page
+            <TextField
+                hintText="Login"
+            />
+            <TextField
+                hintText="Password"
+                type="password"
+            />
+            <RaisedButton
+                label="Login"
+                primary={true}
+                onMouseDown={this.login}/>
+            <Link to="/">
+                <RaisedButton
+                    label="Back"/>
+            </Link>
         </div>
     };
 }
