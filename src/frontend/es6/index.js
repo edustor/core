bab6855039694d3cce23de6185d34afbb1a94c7f
wutiarn <wutiarn@wutiarn-nb.lan>
2016-02-24 18:06:02 +0300

    MaterialUI

--- src/frontend/es6/index.js
+++ src/frontend/es6/index.js
@@ -2,13 +2,13 @@ require("./styl/index");
 
 import ReactDom from 'react-dom';
 import React from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import RaisedButton from 'material-ui/lib/raised-button';
 
-
+injectTapEventPlugin();
 
 ReactDom.render(
-<div id="react_root">
-    Hello world
+    <div id="react_root">
+        <RaisedButton label="Hello" />
     </div>,
-    document.getElementById("container")
-)
-;
+document.getElementById("container"));
