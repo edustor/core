3ebbc089733a25f28d7248adeba05d0c00b1841b
wutiarn <wutiarn@wutiarn-nb.lan>
2016-02-27 18:27:25 +0300

    Cropping experiments

--- backend/src/main/kotlin/ru/wutiarn/edustor/utils/pdfReader.kt
+++ backend/src/main/kotlin/ru/wutiarn/edustor/utils/pdfReader.kt
@@ -5,6 +5,7 @@ import com.google.zxing.NotFoundException
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource
 import com.google.zxing.common.HybridBinarizer
 import com.google.zxing.qrcode.QRCodeReader
+import com.google.zxing.qrcode.detector.Detector
 import org.ghost4j.document.PDFDocument
 import org.ghost4j.renderer.SimpleRenderer
 import org.slf4j.LoggerFactory
@@ -37,7 +38,26 @@ fun processPdfUpload(file: ByteArray): Map<String, ByteArray> {
 
 
         try {
-            val qrResult = codeReader.decode(BinaryBitmap(HybridBinarizer(BufferedImageLuminanceSource(qrImage))))
+
+            val bufferedImageLuminanceSource = BufferedImageLuminanceSource(qrImage, 2000, 2900, 400, 400)
+
+            val clazz = bufferedImageLuminanceSource.javaClass
+            val field = clazz.getDeclaredField("image")
+            field.isAccessible = true
+            val cropped = field.get(bufferedImageLuminanceSource) as BufferedImage
+
+            FileOutputStream("${i}_p.png").use { it.write(getImageAsByteArray(cropped)) }
+
+            logger.info("Cropped saved")
+
+
+            val hybridBinarizer = HybridBinarizer(bufferedImageLuminanceSource)
+
+            val binaryBitmap = BinaryBitmap(hybridBinarizer)
+
+            Detector(binaryBitmap.blackMatrix).detect()
+
+            val qrResult = codeReader.decode(binaryBitmap)
             val uuid = qrResult.text
 
             logger.info("found: $uuid")
