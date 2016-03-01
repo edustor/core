bc111a9e02f2be00652aa765285ca1d751f8771b
wutiarn <wutiarn@Air-wutiarn.lan>
2016-03-01 10:28:20 +0300

    java.time serialization

--- backend/src/main/kotlin/ru/wutiarn/edustor/EdustorApplication.kt
+++ backend/src/main/kotlin/ru/wutiarn/edustor/EdustorApplication.kt
@@ -6,6 +6,4 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
  * Created by wutiarn on 22.02.16.
  */
 @SpringBootApplication
-open class EdustorApplication {
-
-}
+open class EdustorApplication
