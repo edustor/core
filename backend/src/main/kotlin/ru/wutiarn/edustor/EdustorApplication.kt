dc927b701ccec5b133d2b431d654264deb6af08d
Dmitry Romanov <me@wutiarn.ru>
2016-03-07 01:38:01 +0300

    Application UTC timezone

--- backend/src/main/kotlin/ru/wutiarn/edustor/EdustorApplication.kt
+++ backend/src/main/kotlin/ru/wutiarn/edustor/EdustorApplication.kt
@@ -1,9 +1,14 @@
 package ru.wutiarn.edustor
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import java.util.*
 
 /**
  * Created by wutiarn on 22.02.16.
  */
 @SpringBootApplication
-open class EdustorApplication
+open class EdustorApplication {
+    init {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+    }
+}
