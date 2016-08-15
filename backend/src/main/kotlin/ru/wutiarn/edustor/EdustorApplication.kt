5c67a9f4d2fdcabb18d7758ea33dc48a0708ac91
Dmitry Romanov <me@wutiarn.ru>
2016-08-15 19:19:29 +0300

    Version bump

--- backend/src/main/kotlin/ru/wutiarn/edustor/EdustorApplication.kt
+++ backend/src/main/kotlin/ru/wutiarn/edustor/EdustorApplication.kt
@@ -22,7 +22,7 @@ import java.util.*
 open class EdustorApplication : RabbitListenerConfigurer, WebMvcConfigurerAdapter() {
 
     companion object {
-        val VERSION: String = "0.4.2.4"
+        val VERSION: String = "0.4.3"
     }
 
     @Autowired lateinit var fcmInterceptor: FCMInterceptor
