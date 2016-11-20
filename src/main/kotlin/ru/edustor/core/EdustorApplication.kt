6ad9c3013535796a0c05974e7649652829775701
Dmitry Romanov <me@wutiarn.ru>
2016-11-20 23:47:44 +0300

    v0.5.0.5 bump

--- src/main/kotlin/ru/edustor/core/EdustorApplication.kt
+++ src/main/kotlin/ru/edustor/core/EdustorApplication.kt
@@ -17,7 +17,7 @@ import javax.servlet.Filter
 open class EdustorApplication : WebMvcConfigurerAdapter() {
 
     companion object {
-        val VERSION: String = "0.5.0.4"
+        val VERSION: String = "0.5.0.5"
     }
 
     @org.springframework.beans.factory.annotation.Autowired lateinit var fcmInterceptor: FCMInterceptor
