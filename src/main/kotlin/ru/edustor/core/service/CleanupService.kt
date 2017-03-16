e6e2d306e02c7c474e2c5db9dbf89e4b2481ba65
Dmitry Romanov <me@wutiarn.ru>
2017-03-16 20:50:43 +0300

    Fix CleanupService

--- src/main/kotlin/ru/edustor/core/service/CleanupService.kt
+++ src/main/kotlin/ru/edustor/core/service/CleanupService.kt
@@ -3,6 +3,7 @@ package ru.edustor.core.service
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
 import ru.edustor.commons.storage.service.BinaryObjectStorageService
 import ru.edustor.commons.storage.service.BinaryObjectStorageService.ObjectType.PAGE
 import ru.edustor.core.repository.AccountRepository
@@ -11,7 +12,7 @@ import java.time.Instant
 import java.time.temporal.ChronoUnit
 import javax.annotation.PostConstruct
 
-//@Service
+@Service
 open class CleanupService(
         val lessonRepository: LessonRepository,
         val pdfStorage: BinaryObjectStorageService,
@@ -30,17 +31,27 @@ open class CleanupService(
 //        Cleanup tags
         accountRepository.findByTagsRemovedOnLessThan(cleanupBeforeDate)
                 .map { account ->
-                    account.tags = account.tags.filter { it.removedOn!! < cleanupBeforeDate }.toMutableList()
+                    account.tags = account.tags
+                            .filter {
+                                val ok = it.removedOn?.isAfter(cleanupBeforeDate) ?: true
+                                if (!ok) {
+                                    logger.info("Cleanup tag: ${it.id}")
+                                }
+                                return@filter ok
+                            }
+                            .toMutableList()
                     accountRepository.save(account)
                 }
 
 //        Cleanup lessons
+//        TODO: Cleanup empty lessons
         lessonRepository.findByRemovedOnLessThan(cleanupBeforeDate)
                 .map { lesson ->
                     lesson.pages.forEach { page ->
                         if (page.isUploaded) {
                             pdfStorage.delete(PAGE, page.fileId!!)
                         }
+                        logger.info("Cleanup lesson: ${lesson.id}")
                         lessonRepository.delete(lesson)
                     }
                 }
@@ -48,7 +59,15 @@ open class CleanupService(
 //        Cleanup pages
         lessonRepository.findByPagesRemovedOnLessThan(cleanupBeforeDate)
                 .map { lesson ->
-                    lesson.pages.filter { it.removedOn!! < cleanupBeforeDate }.toMutableList()
+                    lesson.pages = lesson.pages
+                            .filter {
+                                val ok = it.removedOn?.isAfter(cleanupBeforeDate) ?: true
+                                if (!ok) {
+                                    logger.info("Cleanup page: ${it.id}")
+                                }
+                                return@filter ok
+                            }
+                            .toMutableList()
                     lessonRepository.save(lesson)
                 }
 
