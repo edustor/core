428b110bbc5c0cc30a1988f3914ed6f064511ce0
Dmitry Romanov <me@wutiarn.ru>
2017-04-23 22:58:48 +0300

    Use new Edustor Auth

--- src/main/kotlin/ru/edustor/core/util/EdustorCoreAccountResolver.kt
+++ src/main/kotlin/ru/edustor/core/util/EdustorCoreAccountResolver.kt
@@ -5,21 +5,21 @@ import org.springframework.web.bind.support.WebDataBinderFactory
 import org.springframework.web.context.request.NativeWebRequest
 import org.springframework.web.method.support.ModelAndViewContainer
 import ru.edustor.commons.auth.EdustorTokenValidator
-import ru.edustor.commons.auth.internal.EdustorAccountResolver
-import ru.edustor.commons.models.internal.accounts.EdustorAccount
+import ru.edustor.commons.auth.internal.EdustorAuthProfileResolver
+import ru.edustor.commons.auth.model.EdustorAuthProfile
 import ru.edustor.core.model.Account
 import ru.edustor.core.repository.AccountRepository
 
 open class EdustorCoreAccountResolver(val repo: AccountRepository,
-                                      validator: EdustorTokenValidator) : EdustorAccountResolver(validator) {
+                                      validator: EdustorTokenValidator) : EdustorAuthProfileResolver(validator) {
     override fun supportsParameter(parameter: MethodParameter): Boolean {
         return parameter.parameterType == Account::class.java
     }
 
     override fun resolveArgument(parameter: MethodParameter?, mavContainer: ModelAndViewContainer?, webRequest: NativeWebRequest, binderFactory: WebDataBinderFactory?): Any? {
-        val protoAccount = super.resolveArgument(parameter, mavContainer, webRequest, binderFactory) as EdustorAccount
-        val account = repo.findOne(protoAccount.uuid)
+        val authProfile = super.resolveArgument(parameter, mavContainer, webRequest, binderFactory) as EdustorAuthProfile
+        val account = repo.findOne(authProfile.accountId)
 
-        return account ?: Account(protoAccount.uuid)
+        return account ?: Account(authProfile.accountId)
     }
 }
