9a2f783b49c5a396e309698683456e98f1162738
Dmitry Romanov <me@wutiarn.ru>
2017-04-25 22:51:26 +0300

    Redirect / to medium post

--- src/main/kotlin/ru/edustor/core/controller/RootController.kt
+++ src/main/kotlin/ru/edustor/core/controller/RootController.kt
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 class RootController {
     @RequestMapping
     fun root(): String {
-        return "redirect:https://github.com/edustor/core"
+        return "redirect:https://wutiarn.ru/edustor-overview-a8acc7f6e09b"
     }
 }
