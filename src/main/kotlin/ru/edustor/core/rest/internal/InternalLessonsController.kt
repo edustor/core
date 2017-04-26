682c71faa3253b319c3503ac842571c2207638e8
Dmitry Romanov <me@wutiarn.ru>
2017-04-26 15:02:47 +0300

    Internal lesson page files endpoint

---
+++ src/main/kotlin/ru/edustor/core/rest/internal/InternalLessonsController.kt
@@ -0,0 +1,17 @@
+package ru.edustor.core.rest.internal
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import ru.edustor.commons.auth.annotation.RequiresAuth
+import ru.edustor.core.model.Lesson
+import ru.edustor.core.model.Page
+
+@RestController
+@RequestMapping("/api/internal/lessons")
+class InternalLessonsController {
+    @RequestMapping("{lesson}/pages/files")
+    @RequiresAuth("internal")
+    fun lessonPageFiles(lesson: Lesson): List<String> {
+        return lesson.pages.map(Page::fileId).filterNotNull()
+    }
+}
