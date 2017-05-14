81133aeeab64e5dfb8fed088d96d11ecc42ab428
Dmitry Romanov <me@wutiarn.ru>
2017-05-14 10:19:35 +0300

    WIP: Use GDrive

--- src/main/kotlin/ru/edustor/core/rest/internal/InternalLessonsController.kt
+++ src/main/kotlin/ru/edustor/core/rest/internal/InternalLessonsController.kt
@@ -3,6 +3,7 @@ package ru.edustor.core.rest.internal
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.edustor.commons.auth.annotation.RequiresAuth
+import ru.edustor.commons.models.internal.core.LessonPageFilesResponse
 import ru.edustor.core.model.Lesson
 import ru.edustor.core.model.Page
 
@@ -11,9 +12,11 @@ import ru.edustor.core.model.Page
 class InternalLessonsController {
     @RequestMapping("{lesson}/pages/files")
     @RequiresAuth("internal")
-    fun lessonPageFiles(lesson: Lesson): List<String> {
-        return lesson.pages
+    fun lessonPageFiles(lesson: Lesson): LessonPageFilesResponse {
+        val pages = lesson.pages
                 .filter { !it.removed && it.contentType == "application/pdf" }
-                .map(Page::fileId).filterNotNull()
+                .map(Page::fileId)
+                .filterNotNull()
+        return LessonPageFilesResponse(ownerId = lesson.owner.id, pages = pages)
     }
 }
